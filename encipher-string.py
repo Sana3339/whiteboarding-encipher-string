73065a2d6eff456fb45f9fe482c9f238e2cb3e79
Sana <sanaahmad393@gmail.com>
2021-09-11 16:55:58 +0000

    Completed encipher-string.py

--- encipher-string.py
+++ encipher-string.py
@@ -21,17 +21,33 @@ It should preserve capitalization, whitespace, and any special characters:
 def rot_encode(shift, txt):
     """Encode `txt` by shifting its characters to the right."""
 
-    #create a new string to hold our final answer
-    encoded_string = ""
+    alpha_list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-    #create rotary alphanumeric dictionary
-    alpha =
 
-    #for index of each letter in txt, add the shift number to get the index of the new letter
 
-    #use the dictionary to look up index
+    new_txt = []
 
-    #append new letter to our string
+    for char in txt:            #abcxyz, shift =3
+        make_uppercase = False
+        if char.isalpha():      #'x'
+            if char.isupper() == True:
+                make_uppercase = True       #make_uppercase = False
+            char = char.lower()
+            char_idx = alpha_list.index(char)   #char_idx = 23
+            new_char_idx = char_idx + shift     #new_char_idx = 26
+            if new_char_idx >= 26:
+                new_char_idx -= 26
+            new_char = alpha_list[new_char_idx]     #new_char = 'a'
+            if make_uppercase == True:
+                new_char = new_char.upper()
+            new_txt.append(new_char)
+
+        else:                                   #new_txt = ['d', a']
+            new_txt.append(char)
+
+    return ''.join(new_txt)
+
+
 
 
 
